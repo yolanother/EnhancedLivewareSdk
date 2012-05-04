5b80571208e5378d3f8d0ac7a5f358bbcf2efd55
Aaron Jackson <a1.jackson@samsung.com>
2012-05-04 07:33:15 -0500

    Added rough ViewGroup and HorizontalPager controls.

---
+++ src/com/sonyericsson/extras/liveware/extension/util/view/HorizontalPager.java
@@ -0,0 +1,134 @@
+package com.sonyericsson.extras.liveware.extension.util.view;
+
+/*
+ * Modifications by Yoni Samlan; based on RealViewSwitcher, whose license is:
+ *
+ * Copyright (C) 2010 Marc Reichelt
+ *
+ * Work derived from Workspace.java of the Launcher application
+ *  see http://android.git.kernel.org/?p=platform/packages/apps/Launcher.git
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import com.sonyericsson.extras.liveware.aef.control.Control;
+
+import android.content.Context;
+import android.os.Handler;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.view.VelocityTracker;
+import android.view.View;
+import android.view.ViewConfiguration;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.Scroller;
+
+/**
+ * A view group that allows users to switch between multiple screens (layouts) in the same way as
+ * the Android home screen (Launcher application).
+ * <p>
+ * You can add and remove views using the normal methods {@link ViewGroup#addView(View)},
+ * {@link ViewGroup#removeView(View)} etc. You may want to listen for updates by calling
+ * {@link HorizontalPager#setOnScreenSwitchListener(OnScreenSwitchListener)} in order to perform
+ * operations once a new screen has been selected.
+ *
+ * Modifications from original version (ysamlan): Animate argument in setCurrentScreen and duration
+ * in snapToScreen; onInterceptTouchEvent handling to support nesting a vertical Scrollview inside
+ * the RealViewSwitcher; allowing snapping to a view even during an ongoing scroll; snap to
+ * next/prev view on 25% scroll change; density-independent swipe sensitivity; width-independent
+ * pager animation durations on scrolling to properly handle large screens without excessively
+ * long animations.
+ *
+ * Other modifications:
+ * (aveyD) Handle orientation changes properly and fully snap to the right position.
+ *
+ * @author Marc Reichelt, <a href="http://www.marcreichelt.de/">http://www.marcreichelt.de/</a>
+ * @version 0.1.0
+ */
+public class HorizontalPager extends ControlExtensionViewGroup {
+    /*
+     * How long to animate between screens when programmatically setting with setCurrentScreen using
+     * the animate parameter
+     */
+    private static final int INVALID_SCREEN = -1;
+    private int mCurrentScreen;
+
+    /**
+     * Simple constructor to use when creating a view from code.
+     *
+     * @param context The Context the view is running in, through which it can
+     *        access the current theme, resources, etc.
+     */
+    public HorizontalPager(final Context context, final String hostAppPackageName,
+            Handler handler) {
+        super(context, hostAppPackageName, handler);
+        init();
+    }
+    
+    /**
+     * Sets up the scroller and touch/fling sensitivity parameters for the pager.
+     */
+    private void init() {
+        // Calculate the density-dependent snap velocity in pixels
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        ((WindowManager) getContext().getSystemService(Context.WINDOW_SERVICE)).getDefaultDisplay()
+                .getMetrics(displayMetrics);
+    }
+
+    @Override
+    protected void onLayout(final boolean changed, final int l, final int t, final int r,
+            final int b) {
+        int childLeft = 0;
+        final int count = getChildCount();
+
+        for (int i = 0; i < count; i++) {
+            final View child = getChildAt(i);
+            if (child.getVisibility() != View.GONE) {
+                final int childWidth = child.getMeasuredWidth();
+                child.layout(childLeft, 0, childLeft + childWidth, child.getMeasuredHeight());
+                childLeft += childWidth;
+            }
+        }
+    }
+    
+    @Override
+    public void onSwipe(int direction) {
+        super.onSwipe(direction);
+        Log.d("HorizontalPager", "Event: " + direction);
+        if(direction == Control.Intents.SWIPE_DIRECTION_RIGHT) {
+            setCurrentScreen(Math.min(mCurrentScreen - 1, 0), false);
+            Log.d("HorizontalPager", "Swipe left");
+        } else if(direction == Control.Intents.SWIPE_DIRECTION_LEFT) {
+            setCurrentScreen(mCurrentScreen + 1, false);
+            Log.d("HorizontalPager", "Swipe right");
+            
+        }
+    }
+    /**
+     * Sets the current screen.
+     *
+     * @param currentScreen The new screen.
+     * @param animate True to smoothly scroll to the screen, false to snap instantly
+     */
+    public void setCurrentScreen(final int currentScreen, final boolean animate) {
+        mCurrentScreen = Math.max(0, Math.min(currentScreen, getChildCount() - 1));
+        if (animate) {
+            //snapToScreen(currentScreen, ANIMATION_SCREEN_SET_DURATION_MILLIS);
+        } else {
+            scrollTo(mCurrentScreen * getWidth(), 0);
+        }
+        invalidate();
+    }
+
+}
